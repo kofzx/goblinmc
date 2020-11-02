7c9caeba071cf19df205e11b5a24cd01141c80d0
e <l>
2020-11-02 14:34:31 +0800

    feat: 单曲下载

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -3,17 +3,36 @@
  * @author kofzx
  */
 
-function downloadFn(content, filename) {
-    // 创建隐藏的可下载链接
-    var eleLink = document.createElement('a')
-    eleLink.download = filename
-    eleLink.style.display = 'none'
-    // 字符内容转变成blob地址
-    var blob = new Blob([content])
-    eleLink.href = URL.createObjectURL(blob)
-    // 触发点击
-    document.body.appendChild(eleLink)
-    eleLink.click()
-    // 然后移除
-    document.body.removeChild(eleLink)
+/**
+ * 使用xml对象发起请求，进行文件下载
+ * @param {string} uri 下载链接
+ * @param {string} callback 下载完毕回调
+ */
+function loadFile(uri, callback) {  
+    if (typeof callback != 'function') {  
+        callback = function(uri) {  
+            console.log(uri)
+        }  
+    }  
+    var xhr = new XMLHttpRequest() 
+    xhr.responseType = 'blob' 
+    xhr.onload = function() {  
+        callback(window.URL.createObjectURL(xhr.response))
+    }  
+    xhr.open('GET', uri, true) 
+    xhr.send()
+}
+
+/**
+ * 下载文件的方法
+ * @param {string} blobUri blob的资源地址
+ * @param {*} filename 下载的文件名
+ */
+function downloadFn(blobUri, filename) {
+    var link = document.createElement('a')
+    link.href = blobUri
+    link.setAttribute('download', filename)
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
 }
