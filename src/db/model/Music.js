ca8614a5f01fd3aa60bf70c78b9e147789b5e142
e <l>
2020-11-04 17:30:34 +0800

    feat: music表字段优化

--- src/db/model/Music.js
+++ src/db/model/Music.js
@@ -17,12 +17,12 @@ const Music = seq.define('music', {
         type: INTEGER,
         allowNull: false,
         defaultValue: 1,
-        comment: '音乐语种 1 中文 2 英语 3 日语 4 其他'
+        comment: '音乐语种 1 中文 2 英语 3 日语 4 韩语 5 法语 6 其他'
     },
     isfree: {
         type: INTEGER,
         allowNull: false,
-        defaultValue: 1,
+        defaultValue: 0,
         comment: '是否免费'
     },
     name: {
@@ -34,41 +34,6 @@ const Music = seq.define('music', {
         type: STRING,
 		allowNull: false,
 		comment: '歌手'
-    },
-    artistid: {
-        type: INTEGER,
-        allowNull: false,
-        comment: '歌手id'
-    },
-    album: {
-        type: STRING,
-        allowNull: false,
-        comment: '专辑'
-    },
-    albumid: {
-        type: INTEGER,
-        allowNull: false,
-        comment: '专辑id'
-    },
-    albumpic: {
-        type: STRING,
-        allowNull: false,
-        comment: '专辑图片'
-    },
-    pic120: {
-        type: STRING,
-        allowNull: false,
-        comment: '封面图片120*120'
-    },
-    duration: {
-        type: INTEGER,
-        allowNull: false,
-        comment: '时长'
-    },
-    score100: {
-        type: STRING,
-        allowNull: false,
-        comment: '评分'
     },
     sourcename: {
         type: STRING,
